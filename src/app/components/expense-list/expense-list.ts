2d2853c1cac34b807f31d63de173aab1912d956f
Nathan Bowman <gr8n8bowman1@gmail.com>
2026-05-06 06:08:33 -0500

    Linked expenses to the expense list and displays them, price, and category

--- src/app/components/expense-list/expense-list.ts
+++ src/app/components/expense-list/expense-list.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
+import { ExpenseService } from "../../services/expense-service";
 
 @Component({
     selector: 'app-expense-list',
     imports:[],
     templateUrl: './expense-list.html'
 })
-export class ExpenseListComponent{}
+export class ExpenseListComponent{
+    constructor(public expenseService: ExpenseService) {}
+}
