674eb055c42bfa7c7b595c35a146a5d9a92c277e
Nathan Bowman <gr8n8bowman1@gmail.com>
2026-05-06 06:46:40 -0500

    Added the options to input values for the edit expense, needs to have the save added still

--- src/app/components/edit-expense/edit-expense.ts
+++ src/app/components/edit-expense/edit-expense.ts
@@ -1,8 +1,33 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { ExpenseService } from "../../services/expense-service";
+import { ExpenseCategory } from "../../models/expense";
 
 @Component({
     selector: 'app-edit-expense',
-    imports: [],
+    imports: [FormsModule],
     templateUrl: './edit-expense.html'
 })
-export class EditExpenseComponent {}
+
+export class EditExpenseComponent {
+    id = '';
+    title = '';
+    amount= 0;
+    category: ExpenseCategory='Personal';
+
+    constructor(
+        public expenseService: ExpenseService,
+        private route: ActivatedRoute
+    ) {
+        this.id = this.route.snapshot.paramMap.get('id') || '';
+
+        const expense = this.expenseService.getExpenseById(this.id);
+
+        if(expense) {
+            this.title = expense.title;
+            this.amount = expense.amount;
+            this.category = expense.category;
+        }
+    }
+}
