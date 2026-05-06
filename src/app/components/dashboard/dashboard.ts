25a8cc575c55fdf406556bdc364fab7d8debb02f
Nathan Bowman <gr8n8bowman1@gmail.com>
2026-05-06 05:25:00 -0500

    Made the dashboard show total, number, highest, and average expense.  Also made total expenses blue

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
+import { ExpenseService } from "../../services/expense-service";
 
 @Component({
     selector: 'app-dashboard',
     imports: [],
     templateUrl: './dashboard.html'
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+    constructor(public expenseService: ExpenseService) {}
+}
