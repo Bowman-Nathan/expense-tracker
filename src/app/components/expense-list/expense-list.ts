4fd29ddb42f07b76e218d25d18f572dcc9b9386b
Nathan Bowman <gr8n8bowman1@gmail.com>
2026-05-06 06:03:21 -0500

    Created view expenses button, tested now adding expenses to the link

---
+++ src/app/components/expense-list/expense-list.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-expense-list',
+    imports:[],
+    templateUrl: './expense-list.html'
+})
+export class ExpenseListComponent{}
