b08bd2a3e04dd754feedd8271ed6e5e06ab62d04
Nathan Bowman <gr8n8bowman1@gmail.com>
2026-05-06 05:46:35 -0500

    Added inputs to the add expense option, allowed for name, amount, and added the selector for the type of expense.

--- src/app/components/add-expense/add-expense.ts
+++ src/app/components/add-expense/add-expense.ts
@@ -1,8 +1,13 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
     selector: 'app-add-expense',
-    imports: [],
+    imports: [FormsModule],
     templateUrl: './add-expense.html'
 })
-export class AddExpenseComponent {}
+export class AddExpenseComponent {
+    title = '';
+    amount = 0;
+    category = 'Personal';
+}
