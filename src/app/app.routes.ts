56e3aa5b990a33afff537ed26aa45ba8de865b7b
Nathan Bowman <gr8n8bowman1@gmail.com>
2026-05-06 05:34:45 -0500

    Added add expense button and linked it to bring to page where expense will be added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard';
+import { AddExpenseComponent } from './components/add-expense/add-expense';
 
 export const routes: Routes = [
-  { path: '', component: DashboardComponent }
+  { path: '', component: DashboardComponent },
+  { path: 'add', component: AddExpenseComponent}
 ];
---
+++ src/app/components/add-expense/add-expense.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-add-expense',
+    imports: [],
+    templateUrl: './add-expense.html'
+})
+export class AddExpenseComponent {}
